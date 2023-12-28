4e2f26a2c618bcbb90be336a608d7edb29928667
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-12-28 18:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let student={
+	name:"hello"
+}
+Object.prototype.getKeys= function () {
+	return Object.keys(this);
+}
+// let ans=student.getKeys();
+// console.log(ans);
